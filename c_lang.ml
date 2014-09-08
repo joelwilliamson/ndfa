d4725d86e937faba315852d0c4d893ce83ff4e42
Joel Williamson <joel.s.williamson@gmail.com>
2014-09-08 14:20:00 -0400

    Added C strings

--- c_lang.ml
+++ c_lang.ml
@@ -27,10 +27,31 @@ let identifiers = Lexer.(
 		|> Ndfa.compile
 	})
 
+(* A C string is formatted as follows: The first character is a ".
+ * This is followed by any number of regular characters (non- \"), pairs of \\
+ * escaped quotes (\"), or other escaped characters \[^\"]. Then is a tailing
+ * quote.
+ * "([^\"]|\\|\"|\[^\"])*"
+ *)
+let strings = Lexer.(
+	let regular c = c <> '"' && c <> '\\'
+	and string_regex = Ndfa.Concat [
+                          Ndfa.String "\"";
+                          Ndfa.Star (Ndfa.Union [
+                                  Ndfa.Class regular;
+                                  Ndfa.String "\\\\";
+                                  Ndfa.String "\\\"";
+                                  Ndfa.Concat [
+                                          Ndfa.String "\\";
+                                          Ndfa.Class regular]]);
+                          Ndfa.String "\""]|> Ndfa.compile in
+	{identifier="string" ;
+	regex = string_regex	})
+
 let strip_whitespace : (Lexer.token list -> Lexer.token list) =
 	List.filter ~f:(fun (t:Lexer.token) -> Lexer.(t.identifier) <> "white")
 
-let c_tokens = List.fold ~init:[whitespace;identifiers]
+let c_tokens = List.fold ~init:[whitespace;identifiers;strings]
 		(* The ordering is important here. Since keywords have the form
 		of identifiers, it is important they come first in the token
 		list, such that the lexer will prefer calling something a
@@ -40,4 +61,4 @@ let c_tokens = List.fold ~init:[whitespace;identifiers]
 
 
 
-let c_string = "while\t{return;} goto lbl; lbl:  \t\ndo static"
+let c_string = "while\t{return;} goto lbl; \"A string with a quote\\\" in it\\n\";lbl:  \t\ndo static"
