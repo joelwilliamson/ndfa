4ae4764184af8df7d947097b1fe68bacea8e96da
Joel Williamson <joel.s.williamson@gmail.com>
2014-09-08 10:09:48 -0400

    Beginnings of a c tokenizer.
    Currently handles keywords, punctuation and whitespace

---
+++ c_lang.ml
@@ -0,0 +1,26 @@
+
+
+let make_keyword key =
+	Lexer.({identifier = key;
+	regex = Ndfa.String key |> Ndfa.compile })
+
+let keywords = In_channel.read_lines "c_keywords" |> List.map ~f:make_keyword
+
+let whitespace =
+	Lexer.({identifier = "white";
+	regex = Ndfa.Several (Ndfa.Class Char.is_whitespace) |> Ndfa.compile })
+
+let make_symbol = function
+	| identifier::sym::[] -> Lexer.({identifier;regex=Ndfa.String sym |> Ndfa.compile})
+	| _ -> failwith "Invalid line"
+
+let symbols = In_channel.read_lines "c_punctuation"
+		|> List.map ~f:(String.split ~on:' ')
+		|> List.map ~f:make_symbol
+
+let strip_whitespace : (Lexer.token list -> Lexer.token list) =
+	List.filter ~f:(fun (t:Lexer.token) -> Lexer.(t.identifier) <> "white")
+
+let c_tokens = List.append keywords (whitespace::symbols)
+
+let c_string = "while\t{return;} goto ; :  \t\ndostatic"
